994db55d3124419903f6ef3386777477d20b4328
anhnda <anhnda@users.noreply.github.com>
2023-09-29 18:54:11 +0700

    Update exportSelectedGeneProteins.py

--- exportSelectedGeneProteins.py
+++ exportSelectedGeneProteins.py
@@ -95,12 +95,11 @@ def export_gene_drug():
 def export_gene_drug_full():
     import joblib
     print("Loading full scores...")
-    drugNames, proteinNames, scores = joblib.load("./HpyerAttentionDTI/tmp/protein2DrugsXF_0%s.npy" % params.GEN_SUFFIX)
+    drugNames, proteinNames, scores = joblib.load("./HyperAttentionDTI/tmp/protein2DrugsXF_0%s.npy" % params.GEN_SUFFIX)
     print("Score shape: ", scores.shape)
     dProtein2Id = dict()
     for i, name in enumerate(proteinNames):
         dProtein2Id[name] = i
-
     def getScores(proteinName):
         proteinid = utils.get_dict(dProtein2Id, proteinName, -1)
         if proteinid != -1:
